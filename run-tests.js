348bc1c54950ea8462cf90936fc4a129d5535057
André Costa Lima <andreclima.pt@gmail.com>
2021-01-28 22:31:14 +0000

    chore: close test server before exiting process

--- run-tests.js
+++ run-tests.js
@@ -4,17 +4,22 @@ const execa = require("execa");
 const createServer = require("./test/server");
 
 async function run() {
-    createServer();
+    const server = createServer();
+    let exitCode;
 
     try {
         const result = await execa("rn-test", process.argv.slice(2), {
             preferLocal: true,
             stdio: ["ignore", "inherit", "inherit"],
         });
-        process.exit(result.exitCode);
+        exitCode = result.exitCode;
     } catch (error) {
-        process.exit(error.exitCode);
+        exitCode = error.exitCode;
     }
+
+    await server.close();
+
+    process.exit(exitCode);
 }
 
 run();
